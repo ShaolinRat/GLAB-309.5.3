3b284fa1188685521c1905e504cb5c92e017645c
ShaolinRat <csprowalps@gmail.com>
2025-01-13 10:59:25 -0500

    adding files

---
+++ src/main/java/com/perscholas/GLAB_309_5_3/Glab30953Application.java
@@ -0,0 +1,13 @@
+package com.perscholas.GLAB_309_5_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Glab30953Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Glab30953Application.class, args);
+	}
+
+}
